3ea94899e7bc4882addbcac6734c56bb919aec61
Ruan Martinelli <martinelliruan@gmail.com>
2019-01-25 02:05:19 -0200

    Create nvm-auto-change.sh

---
+++ scripts/nvm-auto-change.sh
@@ -0,0 +1,25 @@
+# Autmatically changes the Node.js version via nvm when
+# cd-ing into a folder with a .nvmrc file inside.
+# The code below should be placed in a .zshrc, after the nvm initialization.
+
+autoload -U add-zsh-hook
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
+load-nvmrc
+
